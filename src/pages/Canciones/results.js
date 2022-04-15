9e0ece0d266b7e2fd1f86f6a154b268a12290ca4
Giuliano Cappa <cappa.giuliano@gmail.com>
2022-04-15 13:55:04 -0300

    Añadir un mensaje para cuando la búsqueda sea fallida

--- src/pages/Canciones/results.js
+++ src/pages/Canciones/results.js
@@ -2,20 +2,37 @@ import React from 'react'
 import styles from './canciones.module.css'
 
 function Results({ data, setData }) {
-  const expReg = /(\n){3,}/gm
-  let letter = data.lyrics.replace(expReg, '\r')
+  let letter
+
+  if (data?.lyrics) {
+    const expReg = /(\n){3,}/gm
+    letter = data.lyrics.replace(expReg, '\r')
+  }
 
   return (
     <>
       <h1>Resultado</h1>
-      <button className={styles.button} onClick={() => setData({})}>
-        Buscar más canciones
-      </button>
-      <article className={styles.cancion}>
-        <h2>{data.artist}</h2>
-        <h3>{data.title}</h3>
-        <blockquote className={styles.letter}>{letter}</blockquote>
-      </article>
+      {data.hasOwnProperty('lyrics') ? (
+        <>
+          <button className={styles.button} onClick={() => setData({})}>
+            Buscar más canciones
+          </button>
+          <article className={styles.cancion}>
+            <h2>{data.artist}</h2>
+            <h3>{data.title}</h3>
+            <blockquote className={styles.letter}>{letter}</blockquote>
+          </article>
+        </>
+      ) : (
+        <article className={styles.cancion}>
+          <h2>Error</h2>
+          <p>
+            Laxy no puedo encontrar la canción{' '}
+            <span className={styles.datos}>{data.title}</span> interpretada por{' '}
+            <span className={styles.datos}>{data.artist}</span>
+          </p>
+        </article>
+      )}
     </>
   )
 }
---
+++ src/pages/Series/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Series() {
+  return (
+    <>
+      <form></form>
+    </>
+  )
+}
+
+export default Series
